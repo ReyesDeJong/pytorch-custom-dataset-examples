3ec1b88f6d188e8054002d324bc7786cc555d26c
ReyesDeJong <reyes.dejong@gmail.com>
2019-06-20 14:41:42 -0400

    parallel time

--- src/parallel.py
+++ src/parallel.py
@@ -47,10 +47,11 @@ class Model(nn.Module):
 
 
 model = Model(input_size, output_size)
-if torch.cuda.device_count() > 1:
-  print("Let's use", torch.cuda.device_count(), "GPUs!")
-  # dim = 0 [30, xxx] -> [10, ...], [10, ...], [10, ...] on 3 GPUs
-  model = nn.DataParallel(model)
+# if torch.cuda.device_count() > 1:
+#   print("Let's use", torch.cuda.device_count(), "GPUs!")
+#   # dim = 0 [30, xxx] -> [10, ...], [10, ...], [10, ...] on 3 GPUs
+#   model = nn.DataParallel(model)
+
 
 model.to(device)
 
